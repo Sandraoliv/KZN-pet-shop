33a3ee94cad63a88bafb609476f656d5d81ad616
dnapolitanno <dudadnapolitano@gmail.com>
2023-03-07 16:24:48 -0300

    style: add style to globalHeader

--- src/components/Header/headerSuccess/headerScuccess.tsx
+++ src/components/Header/headerSuccess/headerScuccess.tsx
@@ -1,6 +1,6 @@
 import logo from "../../../assets/KZpet_shop_Logo.svg";
 
-export function successHeader() {
+export function SuccessHeader() {
   <header>
     <img src={logo} alt="" />
   </header>;
