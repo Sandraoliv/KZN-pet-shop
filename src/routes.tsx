99116d1c70cfd0862ab095c694504eaa14c90b07
lucas cardoso <lcardoso662@gmail.com>
2023-03-08 09:44:26 -0400

    feat: Create sucessPage and route SucessPage

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,6 +4,7 @@ import { NotFound } from "./pages/NotFound/notFound";
 import { PaymentPage } from "./pages/PaymentPage/paymentPage";
 import { RegisterPage } from "./pages/RegisterPage";
 import { ShopPage } from "./pages/ShopPage/shopPage";
+import { SucessPage } from "./pages/SucessPage/sucessPage";
 import { CartProvider } from "./provider/CartContext/CartContext";
 import { UserProvider } from "./provider/UserContext/UserContext";
 
@@ -16,6 +17,7 @@ export const RouterComponent = () => {
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/payment" element={<PaymentPage />} />
+          <Route path="/sucess" element={<SucessPage />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </UserProvider>
