4cfaa0e30d2887e15ee085866b62eda7a0f19be7
dnapolitanno <dudadnapolitano@gmail.com>
2023-03-08 13:22:12 -0300

    feat: create style and add library Material UI

--- src/components/Form/RegisterForm/registerForm.tsx
+++ src/components/Form/RegisterForm/registerForm.tsx
@@ -6,6 +6,7 @@ import { registerFormSchema } from "./validations";
 import { IRegisterFormValues } from "../../../provider/UserContext/@Types";
 import { GlobalHeader } from "../../Header/globalHeader/globalHeader";
 import imgFormRegister from "../../../assets/imgFormRegister.svg";
+import { MainFormRegister, SectionFormRegister } from "./style";
 
 export function RegisterForm() {
   const { registerUser } = useContext(UserContext);
@@ -24,13 +25,10 @@ export function RegisterForm() {
   };
 
   return (
-    <>
+    <SectionFormRegister>
       <GlobalHeader />
-      <main>
-        <section>
-          <p>Doe e salve a vida de um pet,todos juntos para salvar uma vida</p>
-          <img src={imgFormRegister} alt="" />
-        </section>
+      <MainFormRegister>
+        <img src={imgFormRegister} alt="" />
         <form>
           <div></div>
           <h3>Cadastre-se</h3>
@@ -62,7 +60,7 @@ export function RegisterForm() {
           />
           <span>{errors.confirmPassword?.message}</span>
         </form>
-      </main>
-    </>
+      </MainFormRegister>
+    </SectionFormRegister>
   );
 }
