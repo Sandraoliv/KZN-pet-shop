4e1bf0a5f91acb8dc7687eeeee89b162a94d8a79
dnapolitanno <dudadnapolitano@gmail.com>
2023-03-10 20:32:27 -0300

    refactor: buttons header to Material UI libary

--- src/pages/UserPage/userPage.tsx
+++ src/pages/UserPage/userPage.tsx
@@ -1,11 +1,12 @@
-import { BackgroundPages } from "../../components/Background/BackgroundPages/backgroundPages";
-import { SuccessHeader } from "../../components/Header/headerSuccess/headerScuccess";
+import { UserHeader } from "../../components/Header/userHeader/userHeader";
 import { SectionUserPage } from "./style";
+import Button from "@mui/material/Button";
+import { RxExit } from "react-icons/rx";
 
 export function UserPage() {
   return (
     <SectionUserPage>
-      <SuccessHeader />
+      <UserHeader />
       <h1>Userpage</h1>
     </SectionUserPage>
   );
