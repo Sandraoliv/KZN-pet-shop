140c3af51b9f6811f24b03d1d1f21ff35ee79059
dnapolitanno <dudadnapolitano@gmail.com>
2023-03-10 17:39:35 -0300

    feat

---
+++ src/pages/UserPage/userPage.tsx
@@ -0,0 +1,11 @@
+import { BackgroundPages } from "../../components/Background/BackgroundPages/backgroundPages";
+import { SuccessHeader } from "../../components/Header/headerSuccess/headerScuccess";
+import { SectionUserPage } from "./style";
+
+export function UserPage() {
+  return (
+    <SectionUserPage>
+      <SuccessHeader />
+    </SectionUserPage>
+  );
+}
--- src/routes.tsx
+++ src/routes.tsx
@@ -5,6 +5,7 @@ import { PaymentPage } from "./pages/PaymentPage/paymentPage";
 import { RegisterPage } from "./pages/RegisterPage";
 import { ShopPage } from "./pages/ShopPage/shopPage";
 import { SucessPage } from "./pages/SucessPage/sucessPage";
+import { UserPage } from "./pages/UserPage/userPage";
 import { CartProvider } from "./provider/CartContext/CartContext";
 import { UserProvider } from "./provider/UserContext/UserContext";
 
@@ -18,6 +19,7 @@ export const RouterComponent = () => {
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/payment" element={<PaymentPage />} />
           <Route path="/sucess" element={<SucessPage />} />
+          <Route path="/user" element={<UserPage />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </UserProvider>
